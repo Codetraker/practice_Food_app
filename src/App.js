3e1819c2f41e288afdd3769e14c5902269053b3e
Codetraker <swarajbibhuprasad@gmail.com>
2024-02-05 12:43:56 +0530

    Header and cart UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import { Fragment } from 'react';
+
 import Header from './components/Layout/Header';
 
 function App() {
   return (
-    <React.Fragment>
+    <Fragment>
       <Header />
-    </React.Fragment>
+    </Fragment>
   );
 }
 
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,19 +1,21 @@
-import {Fragment} from 'react';
-import mealPic from '../../assets/meals.jpg';
+import { Fragment } from 'react';
+
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
 
-const Header = props =>{
-    return(
-        <Fragment>
-            <header className={classes.header}>
-                <h1>HotMeals</h1>
-                <button>Cart</button>
-            </header>
-            <div className={classes['main-image']}> {/* Because of '-' we can't use '.',so this is the way */}
-                <img src={mealPic} alt='A table full of food!'/>
-            </div>
-        </Fragment>
-    );
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>HotMeals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
 };
 
 export default Header;
